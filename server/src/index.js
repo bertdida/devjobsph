9c9f857dd190e4e8776e0f719ef1001490745a8f
bertdida <38489033+bertdida@users.noreply.github.com>
2020-11-06 19:37:41 +0800

    Use compression middleware

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const compression = require('compression');
 
 const { getJobs } = require('./db');
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('common'));
+app.use(compression());
 app.use(express.static(clientPath));
 
 app.get('/', (req, res) => {
