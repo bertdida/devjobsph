8d8301d6c88e622626c565d4722a963fe13fe32f
bertdida <38489033+bertdida@users.noreply.github.com>
2020-11-08 17:59:37 +0800

    Accept message argument for Loader component

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,11 +1,21 @@
 import Spinner from 'react-bootstrap/Spinner';
+import PropTypes from 'prop-types';
 
-export function Loader() {
+export function Loader({ message }) {
   return (
-    <div className="d-flex justify-content-center">
+    <div className="d-flex align-items-center justify-content-center">
       <Spinner animation="border" role="status" variant="secondary">
         <span className="sr-only">Loading...</span>
       </Spinner>
+      {message && <span className="ml-2">{message}</span>}
     </div>
   );
 }
+
+Loader.propTypes = {
+  message: PropTypes.string,
+};
+
+Loader.defaultProps = {
+  message: null,
+};
